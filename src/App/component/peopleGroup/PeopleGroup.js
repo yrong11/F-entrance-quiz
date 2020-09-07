698fd84921b61f09ba18cca6d600fb2b8c8242d2
yurong <rong.yu@thoughtworks.com>
2020-09-07 22:37:05 +0800

    [yurong] feat: divide people and show group

--- src/App/component/peopleGroup/PeopleGroup.js
+++ src/App/component/peopleGroup/PeopleGroup.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { List } from 'antd';
+
+import People from '../peopleList/People'
 
 class PeopleGroup extends Component {
   render() {
-    const {item, groupName} = this.props
-    console.log(item)
-    console.log(groupName)
+    const { item, groupName } = this.props
+
+    const elements = []
+    item.forEach((itm) => {
+      elements.push(<People key={itm.id} item={itm} />)
+    })
     return (
-    <div className='group-info'>
-      <input onKeyUp={this.props.editGroupName} className="group-name" 
+      <div className='group-info'>
+        <input onKeyUp={this.props.editGroupName} className="group-name"
           type='text' value={groupName} />
-      {item.forEach((itm) => {return(<div>{`${itm.id}.${itm.name}`}</div>)} )}
-    </div>
+          <div className="group-people">
+          <List
+          grid={{ gutter: 16, column: 7 }}
+          dataSource={item}
+          renderItem={itm => (
+            <List.Item>
+              <People
+                key={itm.id}
+                item={itm}
+              />
+            </List.Item>
+          )}
+        />
+          </div>
+        
+      </div>
     );
   }
 }
